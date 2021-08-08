2ad1ce5e72c53da1a17437c8e4a27bfc9465fcef
223yu <223yuta.jp@gmail.com>
2021-08-08 06:36:26 +0000

    #11 make select month form

---
+++ app/assets/javascripts/select-month.js
@@ -0,0 +1,72 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+
+$(document).on('turbolinks:load', function() {
+  let select_count = 0;
+  let select_first_month = 0;
+  let select_second_month = 0;
+
+  // 2月目選択後の処理を関数化
+  function after_select_second_month(e){
+    // class書き換え
+    $(e).addClass('select-month__label--second');
+    $(e).removeClass('select-month__label');
+    // 2月目を取得
+    select_second_month = $(e).attr('for').substr(3,2);
+    // 1月目と2月目の間の月のclass書き換え
+    let between_count = Math.abs(Number(select_first_month) - Number(select_second_month));
+    let min_count = Math.min(Number(select_first_month), Number(select_second_month));
+    let between_array = [...Array(between_count -1 )].map((_, i) => 'mon' + String(i + min_count + 1));
+    let mon_array = [...Array(12)].map((_, i) => '#month' + String(i + 1));
+    between_array.forEach(function(between){
+      mon_array.forEach(function(mon){
+        if($(mon).attr('for') == between){
+          $(mon).addClass('select-month__label--between');
+          $(mon).removeClass('select-month__label');
+        }
+      });
+    });
+  }
+
+  $('.select-month__label').on('click', function(){
+    if(select_count == 2){
+      if($(this).attr('class') == 'select-month__label--second'){
+        $(this).addClass('select-month__label');
+        $(this).removeClass('select-month__label--second');
+        $('.select-month__label--between').addClass('select-month__label');
+        $('.select-month__label--between').removeClass('select-month__label--between');
+        select_count -= 1;
+      }else{
+        let month_id = "#mon" + String(select_first_month);
+        select_first_month = select_second_month;
+        // 1月目のチェックを外す
+        $(month_id).removeAttr('checked').prop('checked', false).change();
+        // クラス書き換え
+        $('.select-month__label--first').addClass('select-month__label');
+        $('.select-month__label--first').removeClass('select-month__label--first');
+        $('.select-month__label--second').addClass('select-month__label--first');
+        $('.select-month__label--second').removeClass('select-month__label--second');
+        $('.select-month__label--between').addClass('select-month__label');
+        $('.select-month__label--between').removeClass('select-month__label--between');
+        after_select_second_month(this);
+      }
+    }else if(select_count == 1){
+      if($(this).attr('class') == 'select-month__label--first'){
+        $(this).addClass('select-month__label');
+        $(this).removeClass('select-month__label--first');
+        select_count -= 1;
+      }else{
+        after_select_second_month(this);
+        select_count += 1;
+      }
+    }else if(select_count == 0){
+      $(this).addClass('select-month__label--first');
+      $(this).removeClass('select-month__label');
+      select_first_month = $(this).attr('for').substr(3,2);
+      select_count += 1;
+    }
+  });
+});
