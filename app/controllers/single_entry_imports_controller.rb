5aa1fe2a316bacd9f03a79204ab709e90dd23d36
223yu <223yuta.jp@gmail.com>
2021-08-21 01:43:26 +0000

    #94 fix bug

--- app/controllers/single_entry_imports_controller.rb
+++ app/controllers/single_entry_imports_controller.rb
@@ -6,58 +6,63 @@ class SingleEntryImportsController < ApplicationController
   end
 
   def create
-    # 変数定義
-    n = 2 # 取込行
-    success_count = 0 # 成功数
-    error_present = false
-    flash[:danger] = ''
-    # csv取込実行
-    CSV.foreach(params[:file].path, headers: true) do |row|
-      import = Import.new
-      import.user_id = current_user.id
-      if Date.valid_date?(row[0].to_i, row[1].to_i, row[2].to_i)
-        if current_user.year == row[0].to_i
-          import.date = Date.new(row[0].to_i, row[1].to_i, row[2].to_i)
+    # csvファイルが読み込まれた場合のみ処理を行う
+    if params[:file] && File.extname(params[:file].original_filename) == '.csv'
+      # 変数定義
+      n = 2 # 取込行
+      success_count = 0 # 成功数
+      error_present = false
+      flash[:danger] = ''
+      # csv取込実行
+      CSV.foreach(params[:file].path, headers: true) do |row|
+        import = Import.new
+        import.user_id = current_user.id
+        if Date.valid_date?(row[0].to_i, row[1].to_i, row[2].to_i)
+          if current_user.year == row[0].to_i
+            import.date = Date.new(row[0].to_i, row[1].to_i, row[2].to_i)
+          else
+            flash[:danger] << "#{n}行目:現在の年度と異なる年が入力されています。"
+            error_present = true
+          end
         else
-          flash[:danger] << "#{n}行目:現在の年度と異なる年が入力されています。"
+          flash[:danger] <<  "#{n}行目:存在しない日付です。"
           error_present = true
         end
-      else
-        flash[:danger] <<  "#{n}行目:存在しない日付です。"
-        error_present = true
-      end
-      if Account.find_by(user_id: current_user.id, year: current_user.year, code: row[3].to_i).present?
-        import.debit_id = current_user.code_id(row[3].to_i)
-      else
-        flash[:danger] << "#{n}行目:借方コードが正しくありません。"
-        error_present = true
-      end
-      if Account.find_by(user_id: current_user.id, year: current_user.year, code: row[4].to_i).present?
-        import.credit_id = current_user.code_id(row[4].to_i)
-      else
-        flash[:danger] <<  "#{n}行目:貸方コードが正しくありません。"
-        error_present = true
-      end
-      if row[5].to_i > 0
-        import.amount = row[5].to_i
-      else
-        flash[:danger] << "#{n}行目:金額が正しくありません。"
-        error_present = true
-      end
-      if row[6] == nil
-        import.description = ''
-      else
-        import.description = row[6]
-      end
-      if error_present == false
-        if import.save
-          success_count += 1
+        if Account.find_by(user_id: current_user.id, year: current_user.year, code: row[3].to_i).present?
+          import.debit_id = current_user.code_id(row[3].to_i)
+        else
+          flash[:danger] << "#{n}行目:借方コードが正しくありません。"
+          error_present = true
         end
+        if Account.find_by(user_id: current_user.id, year: current_user.year, code: row[4].to_i).present?
+          import.credit_id = current_user.code_id(row[4].to_i)
+        else
+          flash[:danger] <<  "#{n}行目:貸方コードが正しくありません。"
+          error_present = true
+        end
+        if row[5].to_i > 0
+          import.amount = row[5].to_i
+        else
+          flash[:danger] << "#{n}行目:金額が正しくありません。"
+          error_present = true
+        end
+        if row[6] == nil
+          import.description = ''
+        else
+          import.description = row[6]
+        end
+        if error_present == false
+          if import.save
+            success_count += 1
+          end
+        end
+        n += 1
+        error_present = false
       end
-      n += 1
-      error_present = false
+      flash[:success] = "#{success_count}件の仕訳を取り込みました。"
+    else
+      flash[:danger] = 'ファイルを読み込むことができませんでした。'
     end
-    flash[:success] = "#{success_count}件の仕訳を取り込みました。"
     redirect_to single_entry_imports_path
   end
 
@@ -119,6 +124,11 @@ class SingleEntryImportsController < ApplicationController
     end
     flash[:success] = "取込待ち仕訳一覧の仕訳を全て削除しました。"
     redirect_to single_entry_imports_path
+  end
+
+  def download
+    download_file_name = 'app/assets/csv/取込用csvファイル.csv'
+    send_file download_file_name
   end
 
   private
