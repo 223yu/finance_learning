b4c8a90b83ae1cf3ef8019f16056e4499e3f883a
223yu <223yuta.jp@gmail.com>
2021-08-28 05:26:45 +0000

    #131 fix bug

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,12 +1,17 @@
 $(document).on('turbolinks:load', function() {
   $(document).on('click', '.modal__open-btn', function(){
     $('.modal__area').fadeIn();
+    $('.modal__close-btn').fadeIn();
     $('.modal__open-btn').fadeOut();
     return false;
   });
-  $(document).on('click', '.modal__close-btn', function(){
-    $('.modal__area').fadeOut();
-    $('.modal__open-btn').fadeIn();
-    return false;
+  const target = ['.modal__close-btn', '.modal__background'];
+  target.forEach(function(target){
+    $(document).on('click', target, function(){
+      $('.modal__area').fadeOut();
+      $('.modal__close-btn').fadeOut();
+      $('.modal__open-btn').fadeIn();
+      return false;
+    });
   });
 });
